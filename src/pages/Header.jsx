c6ba11634b9bfb52f63ecd497c48ad1b215775e9
AngelR-Dev <111716367+AngelR-Dev@users.noreply.github.com>
2022-10-24 18:32:53 -0500

    cambios de posicionamiento y agregado link
    bg de PokedexById
    Link a Pokedex en Header
    centrado el container de pokedex

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles/Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header-container">
       <div className="header-red">
-        <img src="/title.png" className="header-img"/>
+        <Link to="/pokedex">
+          <img src="/title.png" className="header-img" />
+        </Link>
         <div className="header-black"></div>
       </div>
     </div>
